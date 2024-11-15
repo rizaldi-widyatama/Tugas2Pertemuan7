295c9e060c24dbbf8eb7061326515964b03c7ce4
rizaldi-widyatama <muhammad.rizaldi@widyatama.ac.id>
2024-11-15 20:01:30 +0700

    Tutor 4, 5, 6, 7, 8

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/about', 'Page::about');
+$routes->get('/contact', 'Page::contact');
+$routes->get('/faqs', 'Page::faqs');
+$routes->get('/news', 'News::index');
+// $routes->setAutoRoute(true);
